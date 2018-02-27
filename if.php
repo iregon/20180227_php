3b8a65a0cf5a735b26cb8cf9c1d13f3a44b7845b
iregon <0iregon0@gmail.com>
2018-02-27 12:33:45 +0100

    if + rand()

---
+++ if.php
@@ -0,0 +1,10 @@
+<?php
+    $num = rand(0, 1);
+    // echo $num;
+    if($num == 0) {
+        echo "<h2>E' uscito 0</h2>";
+    }
+    else {
+        echo "<h2 style='color:red'>E' uscito 1</h2>";
+    }
+?>
